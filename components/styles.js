111b7781ecae9da6d8910fd17b52ed2a4201c12a
Thunwara <mtheewara@gmail.com>
2024-02-04 16:20:11 +0700

    first commit

---
+++ components/styles.js
@@ -0,0 +1,10 @@
+// styles.js
+
+export const customHeaderStyle = {
+    height: 150,
+    borderBottomEndRadius: 50,
+    borderBottomStartRadius: 50,
+    backgroundColor: "#00e4d0",
+    shadowColor: "#000000",
+    elevation: 25,
+  };
