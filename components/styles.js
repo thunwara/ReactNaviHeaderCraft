09ac0401aa40823fb3bd5ca37162c0325b532823
Thunwara <mtheewara@gmail.com>
2024-02-28 13:34:20 +0700

    completed

--- components/styles.js
+++ components/styles.js
@@ -2,8 +2,8 @@
 
 export const customHeaderStyle = {
     height: 150,
-    borderBottomEndRadius: 50,
-    borderBottomStartRadius: 50,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
     backgroundColor: "#00e4d0",
     shadowColor: "#000000",
     elevation: 25,
